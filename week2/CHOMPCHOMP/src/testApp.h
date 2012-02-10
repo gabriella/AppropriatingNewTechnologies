5ef1290254e3a35e673b0295021714f540714440
Gabriella Levine <gabriella.levine@gmail.com>
2012-02-09 23:00:16 -0500

    homework

---
+++ week2/CHOMPCHOMP/src/testApp.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxCv.h"
+#include "ofxFaceTracker.h"
+using namespace ofxCv;
+using namespace cv;
+
+class testApp : public ofBaseApp {
+public:
+	void setup();
+	void update();
+	void draw();
+	void keyPressed(int key);
+	
+	ofVideoGrabber cam;
+	ofxFaceTracker tracker;
+		
+	ofEasyCam easyCam;
+    ofImage myImage; 
+    ofPolyline mouth;
+   // ofxFaceTracker destTracker;
+
+};
